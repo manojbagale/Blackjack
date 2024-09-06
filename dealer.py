d3cd30865662b2f7a39e8a1547c3ef69091e74c1
Manoj Bagale <95699537+manojbagale@users.noreply.github.com>
2024-09-05 23:00:08 -0400

    push 3

--- dealer.py
+++ dealer.py
@@ -2,4 +2,18 @@
 # Use randint to generate random cards. 
 from random import randint
 # Write all of your part 2A code below this comment. DO NOT CHANGE OR REMOVE THIS COMMENT.
-print("sdfdsfsdsdsfdsfdsfdsfdfds")
+user_number = randint(1, 13)
+if user_number == 1:
+    print("Drew an Ace")
+elif user_number>= 2 and user_number<= 10 and not user_number == 8:
+    print(f"Drew a {user_number}")
+elif user_number == 8:
+    print(f"Drew an 8")
+elif user_number == 11:
+    print("Drew a Jack")
+elif user_number == 12:
+    print("Drew a Queen")
+elif user_number == 13:
+    print("Drew a King")
+else:
+    print("BAD CARD")
