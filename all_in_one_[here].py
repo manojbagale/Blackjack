5d67fb9cdbdb9bf581d66d38e5be1b3db7f3dcae
Manoj Bagale <95699537+manojbagale@users.noreply.github.com>
2024-09-23 15:39:53 -0400

    Update all_in_one_[here].py

--- all_in_one_[here].py
+++ all_in_one_[here].py
@@ -1,85 +1,89 @@
-# DO NOT CHANGE OR REMOVE THIS COMMENT, and do not change this import otherwise all tests will fail.
+ DO NOT CHANGE OR REMOVE THIS COMMENT, and do not change this import otherwise all tests will fail.
+# Use randint to generate random cards. 
 from random import randint
-
 # Write all of your part 3 code below this comment. DO NOT CHANGE OR REMOVE THIS COMMENT.
-print("-----------")
+
+# The user's starting hand has 2 cards.
+print('-----------')
 print('YOUR TURN')
 print('-----------')
-# Draw 1st card.
-card1_rank = randint(1, 13)
-if card1_rank == 1:
+
+# A starting hand has two cards.
+card_rank = randint(1, 13)
+if card_rank == 1:
   # A 1 stands for an ace.
-  card1_name = "Ace"
-elif card1_rank == 11:
+  card_name = 'Ace'
+elif card_rank == 11:
   # An 11 stands for a jack.
-  card1_name = "Jack"
-elif card1_rank == 12:
+  card_name = 'Jack'
+elif card_rank == 12:
   # A 12 stands for a queen.
-  card1_name = "Queen"
-elif card1_rank == 13:
+  card_name = 'Queen'
+elif card_rank == 13:
   # A 13 stands for a king.
-  card1_name = "King"
-else:
-  # All other cards are named by their number, or rank.
-  card1_name = str(card1_rank)
-
-if card1_rank == 1 or card1_rank == 8:
-  print('Drew an ' + card1_name)
-else:
-  print('Drew a ' + card1_name)
-
-if card1_rank == 11 or card1_rank == 12 or card1_rank == 13:
-  # Jacks, Queens, and Kings are worth 10.
-  card1_value = 10
-elif card1_rank == 1:
-  # Aces are worth 11.
-  card1_value = 11
-else:
-  # All other cards are worth the same as their rank.
-  card1_value = card1_rank
-
-# Draw 2nd card.
-card2_rank = randint(1, 13)
-if card2_rank == 1:
-  # A 1 stands for an ace.
-  card2_name = "Ace"
-elif card2_rank == 11:
-  # An 11 stands for a jack.
-  card2_name = "Jack"
-elif card2_rank == 12:
-  # A 12 stands for a queen.
-  card2_name = "Queen"
-elif card2_rank == 13:
-  # A 13 stands for a king.
-  card2_name = "King"
+  card_name = 'King'
 else:
   # All other cards are named by their
   # number, or rank.
-  card2_name = str(card2_rank)
+  card_name = card_rank
 
-if card2_rank == 1 or card2_rank == 8:
-  print('Drew an ' + card2_name)
+if card_rank == 1 or card_rank == 8:
+    print('Drew an ' + str(card_name))
 else:
-  print('Drew a ' + card2_name)
+    print('Drew a ' + str(card_name))
 
-if card2_rank == 11 or card2_rank == 12 or card2_rank == 13:
+if card_rank == 11 or card_rank == 12 or card_rank == 13:
+  # Jacks, Queens, and Kings are worth 10.
+  card1_value = 10
+elif card_rank == 1:
+  # Aces are worth 11.
+  card1_value = 11
+else:
+  # All other cards are worth the same as
+  # their rank.
+  card1_value = card_rank
+
+card_rank = randint(1, 13)
+if card_rank == 1:
+  # A 1 stands for an ace.
+  card_name = 'Ace'
+elif card_rank == 11:
+  # An 11 stands for a jack.
+  card_name = 'Jack'
+elif card_rank == 12:
+  # A 12 stands for a queen.
+  card_name = 'Queen'
+elif card_rank == 13:
+  # A 13 stands for a king.
+  card_name = 'King'
+else:
+  # All other cards are named by their
+  # number, or rank.
+  card_name = card_rank
+
+if card_rank == 1 or card_rank == 8:
+    print('Drew an ' + str(card_name))
+else:
+    print('Drew a ' + str(card_name))
+
+if card_rank == 11 or card_rank == 12 or card_rank == 13:
   # Jacks, Queens, and Kings are worth 10.
   card2_value = 10
-elif card2_rank == 1:
+elif card_rank == 1:
   # Aces are worth 11.
   card2_value = 11
 else:
-  # All other cards are worth the same as their rank.
-  card2_value = card2_rank
+  # All other cards are worth the same as
+  # their rank.
+  card2_value = card_rank
 
-hand_value = card1_value + card2_value
+user_hand = card1_value + card2_value
 
 # The user has the option to keep drawing if their hand is below 21.
-is_yes = True
-while hand_value < 21 and is_yes:
-  should_hit = input('You have ' + str(hand_value) + ". Hit (y/n)? ")
+while user_hand < 21:
+  should_hit = input('You have ' + str(user_hand) + ". Hit (y/n)? ")
   if should_hit == 'n':
-    is_yes = False
+    break
   elif should_hit == 'y':
     card_rank = randint(1, 13)
     if card_rank == 1:
@@ -114,46 +118,122 @@ while hand_value < 21 and is_yes:
         # All other cards are worth the same as their rank.
         card_value = card_rank
 
-    hand_value = hand_value + card_value
+    user_hand = user_hand + card_value
   else:
     print("Sorry I didn't get that.")
 
-print("Final hand: " + str(hand_value) + ".")
+print('Final hand: ' + str(user_hand) + '.')
+if user_hand == 21:
+  print('BLACKJACK!')
+elif user_hand > 21:
+  # The user busts - or immediately loses - if
+  # their hand total goes above 21.
+  print('BUST.')
 
-if hand_value == 21:
-    print("BLACKJACK!")
-elif hand_value > 21:
-    print("BUST.")
-
-print("-----------")
+# The dealer's starting hand has 2 cards.
+print('-----------')
 print('DEALER TURN')
 print('-----------')
 
-dealer_hand_value = 0
-num_cards = 0
-while dealer_hand_value < 17:
+card_rank = randint(1, 13)
+if card_rank == 1:
+  # A 1 stands for an ace.
+  card_name = 'Ace'
+elif card_rank == 11:
+  # An 11 stands for a jack.
+  card_name = 'Jack'
+elif card_rank == 12:
+  # A 12 stands for a queen.
+  card_name = 'Queen'
+elif card_rank == 13:
+  # A 13 stands for a king.
+  card_name = 'King'
+else:
+  # All other cards are named by their
+  # number, or rank.
+  card_name = card_rank
+
+if card_rank == 1 or card_rank == 8:
+  print('Drew an ' + str(card_name))
+else:
+  print('Drew a ' + str(card_name))
+
+if card_rank == 11 or card_rank == 12 or card_rank == 13:
+  # Jacks, Queens, and Kings are worth 10.
+  card1_value = 10
+elif card_rank == 1:
+  # Aces are worth 11.
+  card1_value = 11
+else:
+  # All other cards are worth the same as
+  # their rank.
+  card1_value = card_rank
+
+card_rank = randint(1, 13)
+if card_rank == 1:
+  # A 1 stands for an ace.
+  card_name = 'Ace'
+elif card_rank == 11:
+  # An 11 stands for a jack.
+  card_name = 'Jack'
+elif card_rank == 12:
+  # A 12 stands for a queen.
+  card_name = 'Queen'
+elif card_rank == 13:
+  # A 13 stands for a king.
+  card_name = 'King'
+else:
+  # All other cards are named by their
+  # number, or rank.
+  card_name = card_rank
+
+if card_rank == 1 or card_rank == 8:
+  print('Drew an ' + str(card_name))
+else:
+  print('Drew a ' + str(card_name))
+
+if card_rank == 11 or card_rank == 12 or card_rank == 13:
+  # Jacks, Queens, and Kings are worth 10.
+  card2_value = 10
+elif card_rank == 1:
+  # Aces are worth 11.
+  card2_value = 11
+else:
+  # All other cards are worth the same as
+  # their rank.
+  card2_value = card_rank
+
+dealer_hand = card1_value + card2_value
+
+# The dealer plays through a round of blackjack.
+while dealer_hand < 17:
+  # Per the rules of blackjack, the dealer must
+  # continue drawing cards until their hand
+  # total reaches 17.
+  print('Dealer has ' + str(dealer_hand) + '.')
+
   card_rank = randint(1, 13)
-  num_cards += 1
   if card_rank == 1:
     # A 1 stands for an ace.
-    card_name = "Ace"
+    card_name = 'Ace'
   elif card_rank == 11:
     # An 11 stands for a jack.
-    card_name = "Jack"
+    card_name = 'Jack'
   elif card_rank == 12:
     # A 12 stands for a queen.
-    card_name = "Queen"
+    card_name = 'Queen'
   elif card_rank == 13:
     # A 13 stands for a king.
-    card_name = "King"
+    card_name = 'King'
   else:
-    # All other cards are named by their number, or rank.
-    card_name = str(card_rank)
+    # All other cards are named by their
+    # number, or rank.
+    card_name = card_rank
 
   if card_rank == 1 or card_rank == 8:
-    print('Drew an ' + card_name)
+    print('Drew an ' + str(card_name))
   else:
-    print('Drew a ' + card_name)
+    print('Drew a ' + str(card_name))
 
   if card_rank == 11 or card_rank == 12 or card_rank == 13:
     # Jacks, Queens, and Kings are worth 10.
@@ -162,44 +242,28 @@ while dealer_hand_value < 17:
     # Aces are worth 11.
     card_value = 11
   else:
-    # All other cards are worth the same as their rank.
+    # All other cards are worth the same as
+    # their rank.
     card_value = card_rank
 
-  dealer_hand_value += card_value
-  if num_cards > 1 and dealer_hand_value < 17:
-    print("Dealer has {}.".format(dealer_hand_value))
-  
-print("Final hand: " + str(dealer_hand_value) + ".")
-if dealer_hand_value == 21:
-    print("BLACKJACK!")
-elif dealer_hand_value > 21:
-    print("BUST.")
+  dealer_hand = dealer_hand + card_value
 
-print("-----------")
+print('Final hand: ' + str(dealer_hand) + '.')
+if dealer_hand == 21:
+  print('BLACKJACK!')
+elif dealer_hand > 21:
+  # The dealer busts - or immediately loses - if
+  # their hand total goes above 21.
+  print('BUST.')
+
+# The final game standings.
+print('-----------')
 print('GAME RESULT')
 print('-----------')
 
-if (dealer_hand_value >= 4 and dealer_hand_value < 21) and (hand_value < 21 and hand_value >=4):
-  if dealer_hand_value > hand_value:
-    print("Dealer wins!")
-  elif hand_value > dealer_hand_value:
-    print("You win!")
-  elif hand_value == dealer_hand_value:
-    print("Push.")
-
-elif dealer_hand_value >= 21 and hand_value >= 21:
-
-  if dealer_hand_value == 21 and hand_value != 21:
-    print("Dealer wins!")
-  elif hand_value == 21 and dealer_hand_value != 21:
-    print("You win!")
-
-  if dealer_hand_value > 21 and hand_value > 21:
-    print("Dealer wins!")
-
-elif dealer_hand_value > hand_value:
-  print("Dealer wins!")
-
-elif hand_value > dealer_hand_value:
-  print("You win!")
-
+if (dealer_hand == 21 and user_hand == 21) or (dealer_hand == user_hand and user_hand < 21):
+  print('Push.')
+elif (dealer_hand < 21 and dealer_hand > user_hand) or user_hand > 21 or (dealer_hand == 21 and user_hand != 21):
+  print('Dealer wins!')
+else:
+  print('You win!')
